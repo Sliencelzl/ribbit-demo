4c08b1866f9b6c4bf8fe34f4076ad44a2d1ba534
Sliencelzl <lzl1160016315@163.com>
2022-04-08 08:58:09 +0800

    项目描述

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,34 @@
 import { createStore } from 'vuex'
+//vuex持久化插件
+import createPersistedstate from 'vuex-persistedstate'
+
+import user from './modules/user'
+import cart from './modules/cart'
+import category from './modules/category'
 
 export default createStore({
   state: {
+    
+  },
+  getters:{
+   
   },
   mutations: {
+   
   },
   actions: {
+    
   },
   modules: {
-  }
+    user,
+    cart,
+    category
+  },
+  //配置vuex持久化插件
+  plugins: [
+      createPersistedstate({
+         key: 'erabbit-client-pc-store',
+         paths: ['user', 'cart']
+        })
+      ]
 })
---
+++ src/store/modules/user.js
@@ -0,0 +1,21 @@
+//用户模块
+export default {
+    namespaced:true,
+    state(){
+        return{
+            profile:{
+                id:'',
+                avatar:'',
+                nickname: '',
+                account: '',
+                mobile: '',
+                token: ''
+            }
+        }
+    },
+    mutations:{
+        setUser(state,payload){
+            state.profile = payload
+        }
+    }
+}
